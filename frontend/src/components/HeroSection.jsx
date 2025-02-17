40c9560e071071f2d7682495d760a63274e4ed40
Priyasha-Yadav <priyasha.yadav.cg@gmail.com>
2025-02-17 08:23:20 +0530

    Add HeroSection component: - Introduced HeroSection with centered layout for promotional content - Added 'No. 1 Job Hunt Website' badge with custom styling - Added heading for the main call-to-action: 'Search, Apply & Get Your Dream Jobs' - Included a short description text to support the heading - Placeholder for future job search input field (Mui input)

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search'; // Import the Search icon
+
+function HeroSection() {
+    return (
+        <>
+            <div className="text-center py-16 px-4 bg-gradient-to-r from-[#9C27B0] to-[#F83002] text-white bg-opacity-10">
+                <div className="flex flex-col items-center gap-5 mb-12">
+                    <span className="px-6 py-2 rounded-full font-medium bg-gray-200 text-[#F83002] text-sm">
+                        No. 1 Job Hunt Website
+                    </span>
+                </div>
+                <h1 className="text-4xl sm:text-5xl font-semibold leading-tight mb-4">
+                    Search, Apply & <br /> Get Your <span className="text-[#9C27B0]">Dream Jobs</span>
+                </h1>
+                <p className="text-lg mb-6 text-gray-300">
+                    Find the perfect job for you and kickstart your career with ease.
+                </p>
+                <div className="flex justify-center gap-4">
+                    <TextField
+                        label="Find your dream job"
+                        variant="outlined"
+                        fullWidth
+                        placeholder="Enter job title, skills, or company"
+                        className="max-w-300 rounded-full"
+                        sx={{
+                            '& .MuiOutlinedInput-root': {
+                                borderRadius: '30px',
+                            },
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                        className="text-white px-6 sm:px-8 rounded-full"
+                        sx={{
+                            borderRadius: '30px',
+                        }}
+                    >
+                        <SearchIcon />
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default HeroSection;
